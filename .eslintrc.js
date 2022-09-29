2e2f68db31f1eeb40a9fbfb745af69114c6e16eb
Santese Smith <43831817+santese@users.noreply.github.com>
2022-09-29 12:28:36 -0700

    update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,3 @@
 module.exports = {
-  root: true,
-  extends: ['standard-with-typescript', 'plugin:jest/recommended', 'plugin:jest/style', 'plugin:prettier/recommended'],
-  plugins: ['@typescript-eslint'],
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-    jest: true,
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: 'module',
-    project: './tsconfig.eslint.json',
-  },
-  rules: {
-    '@typescript-eslint/strict-boolean-expressions': 0,
-    '@typescript-eslint/restrict-template-expressions': 0,
-    quotes: 'error',
-  },
+    extends: ['@pliancy/eslint-config-ts'],
 }
--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/*.d.ts'],
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    rootDir: '.',
+    testRegex: '.*\\.spec\\.ts$',
+    transform: {
+        '^.+\\.(t|j)s$': 'ts-jest',
+    },
+    collectCoverageFrom: ['./src/**/*.(t|j)s'],
+    coveragePathIgnorePatterns: ['dist', `\.(?:index)\.ts$`, 'e2e'],
+    coverageDirectory: './coverage',
+    testEnvironment: 'node',
 }
